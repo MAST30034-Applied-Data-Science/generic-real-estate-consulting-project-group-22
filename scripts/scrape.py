7b2d94be1f74ef97ebfe34955b3e920e2dfdb6e3
asitu <anhui.situ@student.unimelb.edu.au>
2022-09-05 22:54:39 +0800

    Update scrape.py

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -7,22 +7,23 @@ This is by no means production code.
 """
 # built-in imports
 import re
-from json import dump
-import requests
-
 from collections import defaultdict
+from json import dump
+from pprint import pprint
+from urllib.request import urlopen
+from xml.dom.minidom import Attr
+
+import requests
 
 # user packages
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
-
-# define header
-headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko)\
-     Chrome/81.0.4044.141 Safari/537.36"}
 
 # constants
 BASE_URL = "https://www.domain.com.au"
-N_PAGES = range(1, 51) # update this to your liking
+N_PAGES = range(1, 51)  # update this to your liking
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+}
 
 # begin code
 url_links = []
@@ -35,60 +36,64 @@ for page in N_PAGES:
 
     # find the unordered list (ul) elements which are the results, then
     # find all href (a) tags that are from the base_url website.
-    index_links = bs_object \
-        .find(
-            "ul",
-            {"data-testid": "results"}
-        ) \
-        .findAll(
-            "a",
-            href=re.compile(f"{BASE_URL}/*") # the `*` denotes wildcard any
-        )
+    index_links = bs_object.find("ul", {"data-testid": "results"}).findAll(
+        "a", href=re.compile(f"{BASE_URL}/*")  # the `*` denotes wildcard any
+    )
 
     for link in index_links:
         # if its a property address, add it to the list
-        if 'address' in link['class']:
-            url_links.append(link['href'])
+        if "address" in link["class"]:
+            url_links.append(link["href"])
 
 
 # for each url, scrape some basic metadata
 for property_url in url_links[1:]:
-    bs_object = BeautifulSoup(requests.get(property_url, headers=headers).text, "html.parser")
+    bs_object = BeautifulSoup(
+        requests.get(property_url, headers=headers).text, "html.parser"
+    )
 
     # looks for the header class to get property name
-    property_metadata[property_url]['name'] = bs_object \
-        .find("h1", {"class": "css-164r41r"}) \
-        .text
+    property_metadata[property_url]["name"] = bs_object.find(
+        "h1", {"class": "css-164r41r"}
+    ).text
 
     # looks for the div containing a summary title for cost
-    property_metadata[property_url]['cost_text'] = bs_object \
-        .find("div", {"data-testid": "listing-details__summary-title"}) \
-        .text
+    property_metadata[property_url]["cost_text"] = bs_object.find(
+        "div", {"data-testid": "listing-details__summary-title"}
+    ).text
 
+    
     # extract coordinates from the hyperlink provided
     # i'll let you figure out what this does :P
-    property_metadata[property_url]['coordinates'] = [
-        float(coord) for coord in re.findall(
-            r'destination=([-\s,\d\.]+)', # use regex101.com here if you need to
-            bs_object \
-                .find(
-                    "a",
-                    {"target": "_blank", 'rel': "noopener noreferer"}
-                ) \
-                .attrs['href']
-        )[0].split(',')
+    property_metadata[property_url]["coordinates"] = [
+        float(coord)
+        for coord in re.findall(
+            r"destination=([-\s,\d\.]+)",  # use regex101.com here if you need to
+            bs_object.find(
+                "a", {"target": "_blank", "rel": "noopener noreferer"}
+            ).attrs["href"],
+        )[0].split(",")
     ]
 
-    property_metadata[property_url]['rooms'] = [
-        re.findall(r'\d\s[A-Za-z]+', feature.text)[0] for feature in bs_object \
-            .find("div", {"data-testid": "property-features"}) \
-            .findAll("span", {"data-testid": "property-features-text-container"})
-    ]
-
-    property_metadata[property_url]['desc'] = re \
-        .sub(r'<br\/>', '\n', str(bs_object.find("p"))) \
-        .strip('</p>')
+    attr_dict = [feature.text for feature in bs_object.find("div", {"data-testid": "property-features"}\
+        ).findAll("span", {"data-testid": "property-features-text-container"})]
+    if attr_dict == []:
+        property_metadata[property_url]["rooms"] = 'null'
+        property_metadata[property_url]["baths"] = 'null'
+        property_metadata[property_url]["parking"] = 'null'
+        property_metadata[property_url]["area"] = 'null'
+    else:
+        property_metadata[property_url]["rooms"] = re.findall(r"\d\s[A-Za-z]+", attr_dict[0])
+        property_metadata[property_url]["baths"] = re.findall(r"\d\s[A-Za-z]+", attr_dict[1])
+        property_metadata[property_url]["parking"] = re.findall(r"\d\s[A-Za-z]+", attr_dict[2])
+        if len(attr_dict) == 4:
+            property_metadata[property_url]["area"] = attr_dict[3]
+    
+    
+    property_metadata[property_url]["desc"] = re.sub(
+        r"<br\/>", "\n", str(bs_object.find("p"))
+    ).strip("</p>")
 
 # output to example json in data/raw/
-with open('../data/raw/example.json', 'w') as f:
+with open("../data/raw/example.json", "w") as f:
     dump(property_metadata, f)
