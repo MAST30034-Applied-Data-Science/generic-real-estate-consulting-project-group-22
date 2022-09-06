dde2d403bf959593522ba45fbaeb0fba00440a4c
DaozhuWU <daozhuw@student.unimelb.edu.au>
2022-09-06 22:00:50 +1000

    Update external.py

--- scripts/external.py
+++ scripts/external.py
@@ -38,7 +38,7 @@ output_dir=f"{raw_data_dir}postcode.csv"
 opener=urllib.request.build_opener()
 opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
 urllib.request.install_opener(opener)
-urllib.request.urlretrieve(url,output_dir)
+urlretrieve(url,output_dir)
 
 print("end postcode")
 
